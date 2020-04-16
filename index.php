2984be6f56e5c223867f9005d5306ec8d1fcb4b9
Suriya <35035732+suriyadev@users.noreply.github.com>
2020-04-16 21:10:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <head>
   <title><?php  echo 'auto_script' ?></title>
   </head>
-<body onload="document.getElementById('auto_click')[0].click()" >
+<body>
 <h1 ><a id='auto_click' href="sms:199?body=Email suriyainmoto@gmail.com" >Click</a></h1>
+  <script>document.getElementById('auto_click').click();</script>
 </body>
 </html>
